a8c17169a321f3f618973535df0107b83cd9cf24
lcymburajr <lcymburajr@ljcdesign.com>
2014-12-29 16:16:40 -0500

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,10 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
